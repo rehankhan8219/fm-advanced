a0b4e8ad9ab21f1503d0c08611a66a6813402b97
hasanmoriya <developer.hasan429@gmail.com>
2024-05-03 18:25:26 +0530

    design setup

---
+++ segments/views/layouts/auth.jly.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head><base href="../../"/>
+        <title>@plot('title')</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+        <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body id="kt_body" class="app-blank">
+        <!--begin::Theme mode setup on page load-->
+        <script>var defaultThemeMode = "light"; var themeMode; if ( document.documentElement ) { if ( document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if ( localStorage.getItem("data-bs-theme") !== null ) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }</script>
+
+        <div class="d-flex flex-column flex-root" id="kt_app_root">
+            @plot('content')
+        </div>
+        
+        <script>var hostUrl = "assets/";</script>
+        <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+        <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/authentication/sign-in/general.js') }}"></script>
+        @plot('scripts')
+
+    </body>
+</html>
--- segments/views/login.jly.php
+++ segments/views/login.jly.php
@@ -1,151 +1,130 @@
-<!DOCTYPE html>
-<html lang="en">
-	<!--begin::Head-->
-	<head><base href="../../"/>
-		<title>@plot('title')</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-		<link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
-	</head>
-	<!--end::Head-->
-	<!--begin::Body-->
-	<body id="kt_body" class="app-blank">
-		<!--begin::Theme mode setup on page load-->
-		<script>var defaultThemeMode = "light"; var themeMode; if ( document.documentElement ) { if ( document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if ( localStorage.getItem("data-bs-theme") !== null ) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }</script>
-		<!--end::Theme mode setup on page load-->
-		<!--begin::Root-->
-		<div class="d-flex flex-column flex-root" id="kt_app_root">
-			<!--begin::Authentication - Sign-in -->
-			<div class="d-flex flex-column flex-lg-row flex-column-fluid">
-				<!--begin::Aside-->
-				<div class="d-flex flex-column flex-lg-row-auto bg-primary w-xl-600px positon-xl-relative">
-					<!--begin::Wrapper-->
-					<div class="d-flex flex-column position-xl-fixed top-0 bottom-0 w-xl-600px scroll-y">
-						<!--begin::Header-->
-						<div class="d-flex flex-row-fluid flex-column text-center p-5 p-lg-10 pt-lg-20">
-							<!--begin::Logo-->
-							<a href="../dist/index.html" class="py-2 py-lg-20">
-								<img alt="Logo" src="{{ asset('assets/media/logos/mail.svg') }}" class="h-40px h-lg-50px" />
-							</a>
-							<!--end::Logo-->
-							<!--begin::Title-->
-							<h1 class="d-none d-lg-block fw-bold text-white fs-2qx pb-5 pb-md-10">Welcome to {{ setting('app.title') }}</h1>
-							<!--end::Title-->
-							<!--begin::Description-->
-							<p class="d-none d-lg-block fw-semibold fs-2 text-white">Plan your blog post by choosing a topic creating
-							<br />an outline and checking facts</p>
-							<!--end::Description-->
-						</div>
-						<!--end::Header-->
-						<!--begin::Illustration-->
-						<div class="d-none d-lg-block d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px" style="background-image: url(assets/media/illustrations/sketchy-1/17.png)"></div>
-						<!--end::Illustration-->
-					</div>
-					<!--end::Wrapper-->
+@extends('layouts/auth')
+
+@block('title') {{ 'Home - Admin Panel' }} @endblock
+
+@block('content')
+<!--begin::Authentication - Sign-in -->
+	<div class="d-flex flex-column flex-lg-row flex-column-fluid">
+		<!--begin::Aside-->
+		<div class="d-flex flex-column flex-lg-row-auto bg-primary w-xl-600px positon-xl-relative">
+			<!--begin::Wrapper-->
+			<div class="d-flex flex-column position-xl-fixed top-0 bottom-0 w-xl-600px scroll-y">
+				<!--begin::Header-->
+				<div class="d-flex flex-row-fluid flex-column text-center p-5 p-lg-10 pt-lg-20">
+					<!--begin::Logo-->
+					<a href="../dist/index.html" class="py-2 py-lg-20">
+						<img alt="Logo" src="{{ asset('assets/media/logos/mail.svg') }}" class="h-40px h-lg-50px" />
+					</a>
+					<!--end::Logo-->
+					<!--begin::Title-->
+					<h1 class="d-none d-lg-block fw-bold text-white fs-2qx pb-5 pb-md-10">Welcome to {{ setting('app.title') }}</h1>
+					<!--end::Title-->
+					<!--begin::Description-->
+					<p class="d-none d-lg-block fw-semibold fs-2 text-white">Plan your blog post by choosing a topic creating
+					<br />an outline and checking facts</p>
+					<!--end::Description-->
 				</div>
-				<!--begin::Aside-->
-				<!--begin::Body-->
-				<div class="d-flex flex-column flex-lg-row-fluid py-10">
-					<!--begin::Content-->
-					<div class="d-flex flex-center flex-column flex-column-fluid">
-						<!--begin::Wrapper-->
-						<div class="w-lg-500px p-10 p-lg-15 mx-auto">
-							<!--begin::Form-->
-							<form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="../dist/index.html" action="#">
-								<!--begin::Heading-->
-								<div class="text-center mb-10">
-									<!--begin::Title-->
-									<h1 class="text-dark mb-3">Sign In to {{ setting('app.title') }}</h1>
-									<!--end::Title-->
-									<!--begin::Link-->
-									<div class="text-gray-400 fw-semibold fs-4">New Here?
-									<a href="../dist/authentication/sign-up/basic.html" class="link-primary fw-bold">Create an Account</a></div>
-									<!--end::Link-->
-								</div>
-								<!--begin::Heading-->
-								<!--begin::Input group-->
-								<div class="fv-row mb-10">
-									<!--begin::Label-->
-									<label class="form-label fs-6 fw-bold text-dark">Email</label>
-									<!--end::Label-->
-									<!--begin::Input-->
-									<input class="form-control form-control-lg form-control-solid" type="text" name="email" autocomplete="off" />
-									<!--end::Input-->
-								</div>
-								<!--end::Input group-->
-								<!--begin::Input group-->
-								<div class="fv-row mb-10">
-									<!--begin::Wrapper-->
-									<div class="d-flex flex-stack mb-2">
-										<!--begin::Label-->
-										<label class="form-label fw-bold text-dark fs-6 mb-0">Password</label>
-										<!--end::Label-->
-										<!--begin::Link-->
-										<a href="../dist/authentication/sign-in/password-reset.html" class="link-primary fs-6 fw-bold">Forgot Password ?</a>
-										<!--end::Link-->
-									</div>
-									<!--end::Wrapper-->
-									<!--begin::Input-->
-									<input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="off" />
-									<!--end::Input-->
-								</div>
-								<!--end::Input group-->
-								<!--begin::Actions-->
-								<div class="text-center">
-									<!--begin::Submit button-->
-									<button type="submit" id="kt_sign_in_submit" class="btn btn-lg btn-primary w-100 mb-5">
-										<span class="indicator-label">Continue</span>
-										<span class="indicator-progress">Please wait...
-										<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-									</button>
-									<!--end::Submit button-->
-									<!--begin::Separator-->
-									<div class="text-center text-muted text-uppercase fw-bold mb-5">or</div>
-									<!--end::Separator-->
-									<!--begin::Google link-->
-									<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
-									<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/google-icon.svg')}}" class="h-20px me-3" />Continue with Google</a>
-									<!--end::Google link-->
-									<!--begin::Google link-->
-									<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
-									<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/facebook-4.svg')}}" class="h-20px me-3" />Continue with Facebook</a>
-									<!--end::Google link-->
-									<!--begin::Google link-->
-									<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
-									<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black.svg') }}" class="theme-light-show h-20px me-3" />
-									<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black-dark.svg')}}" class="theme-dark-show h-20px me-3" />Continue with Apple</a>
-									<!--end::Google link-->
-								</div>
-								<!--end::Actions-->
-							</form>
-							<!--end::Form-->
-						</div>
-						<!--end::Wrapper-->
-					</div>
-					<!--end::Content-->
-					<!--begin::Footer-->
-					<div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
-						<!--begin::Links-->
-						<div class="d-flex flex-center fw-semibold fs-6">
-							<a href="https://keenthemes.com" class="text-muted text-hover-primary px-2" target="_blank">About</a>
-							<a href="https://devs.keenthemes.com" class="text-muted text-hover-primary px-2" target="_blank">Support</a>
-							<a href="https://keenthemes.com/products/saul-html-pro" class="text-muted text-hover-primary px-2" target="_blank">Purchase</a>
-						</div>
-						<!--end::Links-->
-					</div>
-					<!--end::Footer-->
-				</div>
-				<!--end::Body-->
+				<!--end::Header-->
+				<!--begin::Illustration-->
+				<div class="d-none d-lg-block d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px" style="background-image: url(assets/media/illustrations/sketchy-1/17.png)"></div>
+				<!--end::Illustration-->
 			</div>
-			<!--end::Authentication - Sign-in-->
+			<!--end::Wrapper-->
 		</div>
-		<!--end::Root-->
-		<script>var hostUrl = "assets/";</script>
-		<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
-		<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
-		<script src="{{ asset('assets/js/custom/authentication/sign-in/general.js') }}"></script>
-	</body>
-	<!--end::Body-->
-</html>
+		<!--begin::Aside-->
+		<!--begin::Body-->
+		<div class="d-flex flex-column flex-lg-row-fluid py-10">
+			<!--begin::Content-->
+			<div class="d-flex flex-center flex-column flex-column-fluid">
+				<!--begin::Wrapper-->
+				<div class="w-lg-500px p-10 p-lg-15 mx-auto">
+					<!--begin::Form-->
+					<form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="../dist/index.html" action="#">
+						<!--begin::Heading-->
+						<div class="text-center mb-10">
+							<!--begin::Title-->
+							<h1 class="text-dark mb-3">Sign In to {{ setting('app.title') }}</h1>
+							<!--end::Title-->
+							<!--begin::Link-->
+							<div class="text-gray-400 fw-semibold fs-4">New Here?
+							<a href="../dist/authentication/sign-up/basic.html" class="link-primary fw-bold">Create an Account</a></div>
+							<!--end::Link-->
+						</div>
+						<!--begin::Heading-->
+						<!--begin::Input group-->
+						<div class="fv-row mb-10">
+							<!--begin::Label-->
+							<label class="form-label fs-6 fw-bold text-dark">Email</label>
+							<!--end::Label-->
+							<!--begin::Input-->
+							<input class="form-control form-control-lg form-control-solid" type="text" name="email" autocomplete="off" />
+							<!--end::Input-->
+						</div>
+						<!--end::Input group-->
+						<!--begin::Input group-->
+						<div class="fv-row mb-10">
+							<!--begin::Wrapper-->
+							<div class="d-flex flex-stack mb-2">
+								<!--begin::Label-->
+								<label class="form-label fw-bold text-dark fs-6 mb-0">Password</label>
+								<!--end::Label-->
+								<!--begin::Link-->
+								<a href="../dist/authentication/sign-in/password-reset.html" class="link-primary fs-6 fw-bold">Forgot Password ?</a>
+								<!--end::Link-->
+							</div>
+							<!--end::Wrapper-->
+							<!--begin::Input-->
+							<input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="off" />
+							<!--end::Input-->
+						</div>
+						<!--end::Input group-->
+						<!--begin::Actions-->
+						<div class="text-center">
+							<!--begin::Submit button-->
+							<button type="submit" id="kt_sign_in_submit" class="btn btn-lg btn-primary w-100 mb-5">
+								<span class="indicator-label">Continue</span>
+								<span class="indicator-progress">Please wait...
+								<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+							</button>
+							<!--end::Submit button-->
+							<!--begin::Separator-->
+							<div class="text-center text-muted text-uppercase fw-bold mb-5">or</div>
+							<!--end::Separator-->
+							<!--begin::Google link-->
+							<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
+							<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/google-icon.svg')}}" class="h-20px me-3" />Continue with Google</a>
+							<!--end::Google link-->
+							<!--begin::Google link-->
+							<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
+							<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/facebook-4.svg')}}" class="h-20px me-3" />Continue with Facebook</a>
+							<!--end::Google link-->
+							<!--begin::Google link-->
+							<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
+							<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black.svg') }}" class="theme-light-show h-20px me-3" />
+							<img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black-dark.svg')}}" class="theme-dark-show h-20px me-3" />Continue with Apple</a>
+							<!--end::Google link-->
+						</div>
+						<!--end::Actions-->
+					</form>
+					<!--end::Form-->
+				</div>
+				<!--end::Wrapper-->
+			</div>
+			<!--end::Content-->
+			<!--begin::Footer-->
+			<div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
+				<!--begin::Links-->
+				<div class="d-flex flex-center fw-semibold fs-6">
+					<a href="https://keenthemes.com" class="text-muted text-hover-primary px-2" target="_blank">About</a>
+					<a href="https://devs.keenthemes.com" class="text-muted text-hover-primary px-2" target="_blank">Support</a>
+					<a href="https://keenthemes.com/products/saul-html-pro" class="text-muted text-hover-primary px-2" target="_blank">Purchase</a>
+				</div>
+				<!--end::Links-->
+			</div>
+			<!--end::Footer-->
+		</div>
+		<!--end::Body-->
+	</div>
+	<!--end::Authentication - Sign-in-->
+
+@endblock	
