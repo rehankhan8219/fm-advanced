ded3e794943f791b603a46eceedc17071daee52c
Rehan Masiya <rehanmasiya786@gmail.com>
2024-05-03 15:50:02 +0530

    fm advanced start

--- segments/views/welcome.jly.php
+++ segments/views/welcome.jly.php
@@ -10,6 +10,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
+                    
                     {{ setting('app.title', 'Jolly - A tiny PHP Framework') }}
                 </div>
                 <div class="footer">
